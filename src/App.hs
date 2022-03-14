86b80426fc3e1402b74450f19ac8d1ccc2c717e5
Catherine Galkina <catherine@typeable.io>
2022-03-14 22:02:48 +0300

    UI with placeholders

--- src/App.hs
+++ src/App.hs
@@ -5,6 +5,7 @@ import Control.Monad.IO.Class
 import Data.Bool
 import Data.Functor ((<&>))
 import Data.List as L
+import Data.Maybe hiding (catMaybes)
 import Data.Text as T
 import Data.Witherable (catMaybes)
 import JS
@@ -45,12 +46,11 @@ mainForm = divClass columnClass . divClass "divblockcentered" .
     isEnabled <- Nami.isEnabled
     dWalletConnected <- holdDyn isEnabled (True <$ eConnect) -- make this an input to pass values
     eDeposit <- buttonSwitch Deposit dFormState
-    eConnect <- divClass colCls8 . divClass "appformwrapper" .
-      divClass "form-block w-form" $ do
-        eeConnect <- el "form" . dyn $ dFormState <&> \case
-          Deposit -> depositForm dWalletConnected
-          Withdraw -> withdrawForm >> return never
-        switchHold never eeConnect
+    eConnect <- divClass colCls8 $ do
+      eeConnect <- dyn $ dFormState <&> \case
+        Deposit -> depositForm dWalletConnected
+        Withdraw -> withdrawForm >> return never
+      switchHold never eeConnect
     performEvent_ (Nami.enable <$ eConnect)
     eWithdraw <- buttonSwitch Withdraw dFormState
     blank
@@ -67,32 +67,38 @@ tokenToAmountRange = \case
 data ButtonState = ButtonConnect | ButtonDeposit deriving Eq
 
 depositForm :: MonadWidget t m => Dynamic t Bool -> m (Event t ())
-depositForm dWalletConnected = mdo
-  eToken <- selectInput "Token" tokenHint showToken TokenADA [minBound..]
-  dToken <- holdDyn initToken eToken
-  eAmount <- switchHold never =<< dyn (dToken <&> \tok -> do
-    let
-      range = tokenToAmountRange tok
-      initAmount = tokenInitAmount tok
-    selectInput "Amount" amountHint toText initAmount range)
-  dAmount <- holdDyn (tokenInitAmount initToken) $ leftmost
-    [eAmount, tokenInitAmount <$> eToken]
-  inputTitle "Secret Key" keyHint
-  secretKeyInput (() <$ eDeposit)
-  eeBtn <- divClass "mainbuttonwrapper" $ dyn $ dWalletConnected <&> \case
-    False -> do
-      (e, _) <- elAttr' "a" ("class" =: "button w-button") $
-        text "Connect to wallet"
-      return (ButtonConnect <$ domEvent Click e)
-    True -> do
-      (e, _) <- elAttr' "a" ("class" =: "button w-button") $
-        text "Deposit"
-      return (ButtonDeposit <$ domEvent Click e)
-  eBtn <- switchHold never eeBtn
-  let
-    eConnect = ffilter (== ButtonConnect) eBtn
-    eDeposit = ffilter (== ButtonDeposit) eBtn
-  return (() <$ eConnect)
+depositForm dWalletConnected = do
+  (eConn, eSubmitted) <- divClass "appformwrapper". divClass "form-block w-form"
+    . el "form" $ mdo
+      eToken <- selectInput "Token" tokenHint showToken TokenADA [minBound..]
+      dToken <- holdDyn initToken eToken
+      eAmount <- switchHold never =<< dyn (dToken <&> \tok -> do
+        let
+          range = tokenToAmountRange tok
+          initAmount = tokenInitAmount tok
+        selectInput "Amount" amountHint toText initAmount range)
+      dAmount <- holdDyn (tokenInitAmount initToken) $ leftmost
+        [eAmount, tokenInitAmount <$> eToken]
+      inputTitle "Secret Key" keyHint
+      eTxSubmitted <- secretKeyInput (() <$ eDeposit)
+      eeBtn <- divClass "mainbuttonwrapper" $ dyn $ dWalletConnected <&> \case
+        False -> do
+          (e, _) <- elAttr' "a" ("class" =: "button w-button") $
+            text "Connect to wallet"
+          return (ButtonConnect <$ domEvent Click e)
+        True -> do
+          (e, _) <- elAttr' "a" ("class" =: "button w-button") $
+            text "Deposit"
+          return (ButtonDeposit <$ domEvent Click e)
+      eBtn <- switchHold never eeBtn
+      let
+        eConnect = ffilter (== ButtonConnect) eBtn
+        eDeposit = ffilter (== ButtonDeposit) eBtn
+      return (() <$ eConnect, eTxSubmitted)
+  let elId = "deposit-form-msg"
+  widgetHold_ blank ((elAttr "div" ("class" =: "text-block-2" <> "id" =: elId) $
+    text "Сonfirm the transaction in your wallet") <$ eSubmitted)
+  return eConn
   where
     initToken = TokenADA
     tokenInitAmount tok = let range = tokenToAmountRange tok in
@@ -105,15 +111,17 @@ depositForm dWalletConnected = mdo
     amountHint = "It's an amount"
     keyHint = "It's a secret key"
 
-secretKeyInput :: MonadWidget t m => Event t () -> m ()
+secretKeyInput :: MonadWidget t m => Event t () -> m (Event t ())
 secretKeyInput eDeposit = divClass "secretekeywrapper" . divClass "w-row" $ do
   key :: Integer <- liftIO $ randomRIO (0, p^3)
   performEvent_ (JS.saveTextFile (toText key) <$ eDeposit)
   let keyId = "TextSecreteKey"
   divClass colCls11 $ do
     let
-      keyEnabledAttrs = "class" =: "textform" <> "id" =: keyId
-      keyDisabledAttrs = "class" =: "textform unable"
+      keyEnabledAttrs = "class" =: "textform" <> "id" =: keyId <> style
+      keyDisabledAttrs = "class" =: "textform unable" <> style
+      style = "style" =: "width: 300.66px; word-wrap: inherit; \
+        \white-space: nowrap; overflow: hidden; text-overflow: ellipsis;"
     keyInpAttrs <- holdDyn keyDisabledAttrs (keyEnabledAttrs <$ eDeposit)
     keyInpText <- holdDyn "Please, save the secret key!" (toText key <$ eDeposit)
     elDynAttr "div" keyInpAttrs $ dynText keyInpText
@@ -127,6 +135,7 @@ secretKeyInput eDeposit = divClass "secretekeywrapper" . divClass "w-row" $ do
       ("src" =: "/images/PictCopy.svg" <> "loading" =: "lazy" <>
         "width" =: "23" <> "class" =: "pictcopy") blank
     performEvent_ (copyElemContent keyId <$ domEvent Click e)
+  runDeposit key eDeposit
   where
     colCls11 = "w-col w-col-11 w-col-small-11 w-col-tiny-11"
     colCls4 = "column-4 w-col w-col-1 w-col-small-1 w-col-tiny-1"
@@ -159,23 +168,50 @@ inputTitle title hint = divClass "w-row" $ do
     colCls6 = "w-col w-col-6 w-col-medium-6 w-col-small-6 w-col-tiny-6"
     colCls3 = "column-3 w-col w-col-6 w-col-medium-6 w-col-small-6 w-col-tiny-6"
 
+data WithdrawState
+  = DepositNotFound | DepositFound | WithdrawInProgress
+  | WithdrawOk | WithdrawError
+  deriving (Eq, Show)
+
 withdrawForm :: MonadWidget t m => m ()
-withdrawForm = do
-  inputTitle "Address" addrHint
-  dAddr <- addressInput
-  inputTitle "Secret Key" keyHint
-  dKey <- inputElement $ def
-    & initialAttributes .~ ("class" =: "w-input" <> "type" =: "text" <>
-      "maxlength" =: "256" <>
-      "placeholder" =: "Please, enter the secret key of a deposit")
-  dDepositFound <- holdDyn False never
-  let
-    depBtnAttrs = "class" =: "button w-button" <> "style" =: "cursor:pointer;"
-    btnAttrsHidden = "style" =: "display:none;"
-  eDepBtn <- elDynAttr "a" (bool depBtnAttrs btnAttrsHidden <$> dDepositFound)
-    $ text "Find deposit"
-  -- TODO: find deposit <-> withdraw logic
-  blank
+withdrawForm = divClass "appformwrapper" . divClass "form-block w-form" .
+  el "form" $ mdo
+    inputTitle "Address" addrHint
+    dAddr <- addressInput
+    inputTitle "Secret Key" keyHint
+    dKey <- fmap value $ inputElement $ def
+      & initialAttributes .~ ("class" =: "w-input" <> "type" =: "text" <>
+        "maxlength" =: "256" <>
+        "placeholder" =: "Please, enter the secret key of a deposit")
+    let
+      btnAttrs = "class" =: "button w-button" <> "style" =: "cursor:pointer;"
+      btnAttrsDisabled = "class" =: "button w-button" <> "disabled" =: "" <>
+        "style" =: "color: #c4c4c4;"
+      btnHidden = "style" =: "display: none;"
+      mkDepositAttrs (Just DepositFound) = btnHidden
+      mkDepositAttrs (Just WithdrawInProgress) = btnHidden
+      mkDepositAttrs _ = btnAttrs
+      mkWithdrawAttrs (Just DepositFound) = btnAttrs
+      mkWithdrawAttrs (Just WithdrawInProgress) = btnAttrsDisabled
+      mkWithdrawAttrs _ = btnHidden
+    dWithdrawState <- holdDyn Nothing $ leftmost
+      [ Just DepositFound <$ ffilter isJust eFindDeposit
+      , Just DepositNotFound <$ ffilter isNothing eFindDeposit
+      , Just WithdrawInProgress <$ eWithdraw
+      , Just WithdrawOk <$ ffilter id eWithdrawRes
+      , Just WithdrawError <$ ffilter not eWithdrawRes ]
+    eDeposit <- do
+      (e, _) <- elAttr' "div" ("class" =: "mainbuttonwrapper") . elDynAttr "a"
+        (mkDepositAttrs <$> dWithdrawState) $ text "Find deposit"
+      return (domEvent Click e)
+    eFindDeposit <- findDeposit dAddr dKey eDeposit
+    dDepositRes <- holdDyn Nothing eFindDeposit
+    eWithdraw <- do
+      (e, _) <- elAttr' "div" ("class" =: "mainbuttonwrapper") . elDynAttr "a"
+        (mkWithdrawAttrs <$> dWithdrawState) $ text "Withdraw"
+      return (domEvent Click e)
+    eWithdrawRes <- runWithdraw dAddr dKey eWithdraw
+    blank
   where
     -- TODO: hint texts
     addrHint = "Address"
@@ -216,3 +252,18 @@ safeIndex zs n = guard (n >= 0) >> go zs n
     go [] _ = Nothing
     go  (x:_) 0 = Just x
     go  (_:xs) i = go xs (pred i)
+
+runDeposit :: MonadWidget t m => Integer -> Event t () -> m (Event t ())
+runDeposit _ e = delay 1 e
+-- отправляет запрос к бэкенду, который возвращает String.
+-- Этот String отправляем в balanceTx -> signTx -> submitTx (это все функции API кошелька).
+
+findDeposit :: MonadWidget t m => Dynamic t Text -> Dynamic t Text
+  -> Event t () -> m (Event t (Maybe (Int, Text)))
+findDeposit _dAddr _dKey e = (Just (1, "ADA") <$) <$> delay 1 e
+
+runWithdraw :: MonadWidget t m => Dynamic t Text -> Dynamic t Text
+  -> Event t () -> m (Event t Bool)
+runWithdraw _dAddr _dKey e = (True <$) <$> delay 2 e
+
+
