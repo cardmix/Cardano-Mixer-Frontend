adac466b8749ec354708c3d51cc5764f13ee70e5
cardmix <91214090+cardmix@users.noreply.github.com>
2022-08-03 13:43:18 +0200

    Remove nami-wallet from the source base

--- result/nami-wallet/babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: ['react-app'],
-  plugins: ['react-hot-loader/babel', '@babel/transform-runtime'],
-  env: {
-    test: {
-      presets: ['@babel/preset-env'],
-    },
-  },
-};
